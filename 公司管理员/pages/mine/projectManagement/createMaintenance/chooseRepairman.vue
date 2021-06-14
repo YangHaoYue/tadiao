b1c90810798f132de6ef5391c00348c5e87a9134
邹泽阳 <email@sample.com>
2021-06-14 22:28:53 +0800

    剩下管理员的各种排名

--- 公司管理员/pages/mine/projectManagement/createMaintenance/chooseRepairman.vue
+++ 公司管理员/pages/mine/projectManagement/createMaintenance/chooseRepairman.vue
@@ -1,6 +1,17 @@
 <template>
 	<view>
-		
+		<view class="bg-white u-p-10 u-p-l-30">
+			<u-search class="u-p-10" placeholder="搜索关键字" input-align="left" :focus="true" v-model="keyward" :action-style="{fontWeight:'bold'}"></u-search>
+		</view>
+		<block v-for="(item,index) in list" :key="index">
+			<view class="u-flex u-row-between u-col-center bg-white u-m-t-20" style="padding: 20rpx 30rpx;">
+				<view class="u-flex">
+					<u-avatar :src="item.avatar" size="98"></u-avatar>
+					<view class="u-m-l-16 u-font-24" style="color: #666666;">{{item.name}}</view>
+				</view>
+				<view class="u-font-24" style="color: #1058FB;" @click="back">选择</view>
+			</view>
+		</block>
 	</view>
 </template>
 
@@ -8,15 +19,24 @@
 	export default {
 		data() {
 			return {
-				
+				keyward:'',
+				list:[
+					{avatar:'',name:'张三'},{avatar:'',name:'张三'},{avatar:'',name:'张三'},{avatar:'',name:'张三'},{avatar:'',name:'张三'}
+				]
 			}
 		},
 		methods: {
-			
+			back(){
+				uni.navigateBack({
+					delta: 1
+				});
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style scoped>
+	page{
+		background-color: #F5F6FA;
+	}
 </style>
